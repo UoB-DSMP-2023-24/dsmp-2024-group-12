b732431180ea26c2cd74e56a460dd41ba842e182
xxxxhuai <kehan4476@gmail.com>
2024-02-13 13:12:12 +0000

    embedding

--- encode.py
+++ encode.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
+
+
+
 
 # 读取CSV文件
 df = pd.read_csv('vdj.csv')
@@ -11,7 +14,6 @@ cdr3 = df['cdr3']
 amino_acids = set()
 for sequences in cdr3:
     amino_acids.update(sequences)
-
 
 # 创建氨基酸到索引的映射
 amino_acid_to_index = {amino_acid: i for i, amino_acid in enumerate(amino_acids)}
@@ -32,5 +34,17 @@ def one_hot_encode(sequence, max_length):
 
 # 对每个 CDR3 序列进行 One-Hot 编码和填充
 encoded_cdr3 = [one_hot_encode(seq, max_length) for seq in cdr3]
+one_hot_sequences = torch.stack(encoded_cdr3)
+max_indices = torch.argmax(one_hot_sequences, dim=2)
+
+
+embed = nn.Embedding(len(amino_acids),10)
+outputs = embed(max_indices)
+print(outputs[0])
+# # inputs = torch.LongTensor([[amino_acid_to_index[amino_acid] for amino_acid in eg]])
+# inputs = torch.tensor(encoding)
+# outputs = embed(torch.argmax(inputs, dim=1))
+# print(outputs)
+# # print(embed.weight)
 
 
