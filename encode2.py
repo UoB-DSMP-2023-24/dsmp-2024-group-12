0232c7ec96923d1f5d5d308cabf964dc188688e0
xxxxhuai <kehan4476@gmail.com>
2024-03-15 17:52:32 +0000

    alpha&beta vdj encode

--- encode2.py
+++ encode2.py
@@ -16,8 +16,13 @@ def binary_encoder(data, col):
     return data_sort
 
 col_list = ['v.seg', 'd.segm', 'j.segm']
-for cols in col_list:
-    df_alpha = binary_encoder(df_alpha, cols)
+sub_data = [df_alpha, df_beta]
+names = ['alpha_vdj_encode.csv', 'beta_vdj_encode.csv']
+for sub_df, name in zip(sub_data, names):
+    new_df = sub_df
+    for cols in col_list:
+        new_df = binary_encoder(new_df, cols)
+    new_df.to_csv(name, index=False)
 
 
 # df_alpha_sort = df_alpha.sort_values(by='v.seg')
@@ -25,5 +30,4 @@ for cols in col_list:
 # dfbin = encoder.fit_transform(df_alpha_sort['v.seg'])
 # df_alpha_sort = pd.concat([df_alpha_sort,dfbin],axis=1)
 
-df_alpha.to_csv('alpha_encode_vdj.csv', index=False)
 
